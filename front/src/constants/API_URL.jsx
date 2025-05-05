6fa77993dc4faadb44f2b628aae0bb9585d4696a
Devesh14G <devesh140301gautam>
2025-05-06 01:29:10 +0530

    dist

--- front/src/constants/API_URL.jsx
+++ front/src/constants/API_URL.jsx
@@ -1,10 +1,10 @@
-const API_URL = "http://localhost:3000/api/v1"; 
+// const API_URL = "http://localhost:3000/api/v1"; 
 
 
 
-// const API_URL = 'https://dineoutdevesh-pwql.onrender.com/api/v1';
-// const API_PATH = 'https://dineoutdevesh-pwql.onrender.com';
+const API_URL = 'https://hotelbook-jql4.onrender.com/api/v1';
+const API_PATH = 'https://hotelbook-jql4.onrender.com';
 
-const API_PATH = "http://localhost:3000" ;
+// const API_PATH = "http://localhost:3000" ;
 
 export {API_URL, API_PATH};
