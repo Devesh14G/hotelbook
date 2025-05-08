c73a42548633352bba7d95e407b7efbf4982ecb4
Devesh14G <devesh140301gautam>
2025-05-08 14:38:40 +0530

    url change

--- front/src/constants/API_URL.jsx
+++ front/src/constants/API_URL.jsx
@@ -2,8 +2,8 @@
 
 
 
-const API_URL = 'https://hotelbook-jql4.onrender.com/api/v1';
-const API_PATH = 'https://hotelbook-jql4.onrender.com';
+const API_URL = 'https://hotelbook-vc1y.onrender.com/api/v1';
+const API_PATH = 'https://hotelbook-vc1y.onrender.com';
 
 // const API_PATH = "http://localhost:3000" ;
 
